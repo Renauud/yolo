839c3f9832dcbe7f1ff47ba6a64ef04aafc66856
Renaud <rzell57@mail.com>
2025-01-06 08:35:10 +0100

    heavier and more precise version

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8n.pt")
+model = YOLO("yolov8m.pt")
 
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="data.yaml", epochs=600, imgsz=640)
+results = model.train(data="data.yaml", epochs=100, imgsz=640)
